24a91dbbfe45e8db49091f6d15db412c1eecdaf4
Martín <magadiflo@gmail.com>
2023-05-10 23:38:37 -0500

    Template Driver Form

--- src/app/commons/components/login/login.component.ts
+++ src/app/commons/components/login/login.component.ts
@@ -1,43 +1,40 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { IRequestLogin } from '../../models/auth';
 import { AuthService } from '../../services/auth.service';
 import { JwtAuthService } from '../../services/jwt-auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements AfterViewInit {
+export class LoginComponent {
 
-  @ViewChild('username') username!: ElementRef<HTMLInputElement>;
-  @ViewChild('password') password!: ElementRef<HTMLInputElement>;
+  @ViewChild('myFormLogin') myFormLogin: NgForm | undefined;
+  dataLoginForm: IRequestLogin = { username: '', password: '' };
 
   constructor(
     private router: Router,
     private _loginService: AuthService,
     private _jwtAuthService: JwtAuthService) { }
 
-  ngAfterViewInit(): void {
-    this.username.nativeElement.value = 'john';
-    this.password.nativeElement.value = '123456';
-  }
-
   login(): void {
-    const user: IRequestLogin = {
-      username: this.username.nativeElement.value,
-      password: this.password.nativeElement.value
-    };
-    this._loginService.login(user)
-      .subscribe({
-        next: ({ accessToken }) => {          
-          this._jwtAuthService.login(accessToken);
-          this.router.navigate(['/dashboard']);
-        },
-        error: err => console.log('(error)[LoginComponent]:', err.message),
-      });
+    // const user: IRequestLogin = {
+    //   username: this.username,
+    //   password: this.password
+    // };
+    // this._loginService.login(user)
+    //   .subscribe({
+    //     next: ({ accessToken }) => {
+    //       this._jwtAuthService.login(accessToken);
+    //       this.router.navigate(['/dashboard']);
+    //     },
+    //     error: err => console.log('(error)[LoginComponent]:', err.message),
+    //   });
+    console.log(this.dataLoginForm);
   }
 
 }
--- src/app/commons/shared/auth-components.module.ts
+++ src/app/commons/shared/auth-components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
@@ -14,6 +15,7 @@ import { RegisterComponent } from '../components/register/register.component';
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
   ],
   exports: [
     LoginComponent,
